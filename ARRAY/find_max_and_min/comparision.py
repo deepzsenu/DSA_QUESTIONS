1da52260caaabc2733dfc0db7f83a3ed2c6c9455
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-07-17 18:38:16 +0530

    Create comparision.py

---
+++ ARRAY/find_max_and_min/comparision.py
@@ -0,0 +1,30 @@
+def getminmax(arr):
+	n = len(arr)
+	if n%2==0:
+		mx = max(arr[0], arr[1])
+		mn = min(arr[0], arr[1])
+
+		i = 2
+
+	else:
+		mx, mn = arr[0]
+
+		i = 1
+
+	while (i < n-1):
+		if arr[i] < arr[i+1]:
+			mx = max(mx, arr[i+1])
+			mn = min(mn, arr[i])
+
+		else:
+			mx = max(mx, arr[i])
+
+			mn = min(mn, arr[i+1])
+
+		i+=2
+
+	return (mx, mn)
+
+arr = [20, 1,25, 67,34,56,7,8,88,330,100, 340]
+
+print(getminmax(arr))
